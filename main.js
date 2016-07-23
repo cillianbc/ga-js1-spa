8ebd3424d53a9c4167590c88aa510873730a0c03
Cillianbc <cillianbc@gmail.com>
2016-07-23 13:46:49 +1000

    fetch working

--- main.js
+++ main.js
@@ -1,18 +1,36 @@
-/**
- * Project 3: SPA
- * ====
- *
- * See the README.md for instructions
- */
+(function(){
 
-(function() {
-
-  var container = document.querySelector('#container')
-  var state = {}
-
-  render(state, container)
-
-  function render(data, into) {
-    // TODO
+  var state = {
+    testPage:"http://www.auswax.com.au/",
+    results:{
+      mobile:[],
+      desktop:[],
+    }
   }
+
+  function mobileSpeed(){
+    fetch("https://www.googleapis.com/pagespeedonline/v2/runPagespeed?url="+state.testPage+"&strategy=mobile&key=AIzaSyDfytDoXF01OD9LrVti-BukQjNjxlj2u_I")
+    .then((response)=>{
+      return response.json()
+    }).then((callback)=>{
+      state.results.mobile.push({speedScore:callback.ruleGroups.SPEED.score})
+
+    })
+}
+mobileSpeed()
+  // function desktopSpeed(){
+  //   fetch("https://www.googleapis.com/pagespeedonline/v2/runPagespeed?url="+state.testPage+"&strategy=desktop&key=AIzaSyDfytDoXF01OD9LrVti-BukQjNjxlj2u_I")
+  //   .then((response)=>{
+  //     return response.json()
+  //   }).then((callback)=>{
+  //     state.results.desktop.push({speedScore:callback.ruleGroups.SPEED.score})
+  //   })
+// }
+
+
+  // render(state, container)
+  //
+  // function render(data, into) {
+  //   // TODO
+  // }
 })()
